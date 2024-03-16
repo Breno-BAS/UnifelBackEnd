f2412bd0d9de6dc7b91da514ec4d31a01c6d11e5
Breno Andrade Silva <engbrenoandrade@gmail.com>
2024-03-16 17:26:39 -0300

    Aula de desestruturação

--- unifelBackEnd/script.js
+++ unifelBackEnd/script.js
@@ -1,7 +1,21 @@
-const array = [4, 10, 8, 16, 20]
+const array = ['Amaral', 7829012, 'João', 2931029, 'Regina', 2319993]
 
-const result = array.map((elementoAtual) =>{
-    return elementoAtual * 2
+const variavelQualquer = array.filter((elementoAtual) =>{
+    return elementoAtual >= 7829012
 })
 
-console.log(result)
+console.log(variavelQualquer);
+
+
+
+// Desestruturação de array
+// const array = [
+//     'Computador',
+//     'celular',
+//     'PS5',
+//     'fone'
+// ]
+
+// const [computador, celular, PS5] = array
+
+// console.log(PS5)
