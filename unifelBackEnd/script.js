46c7476b377e36fee9d4a9cacc4c99c6e85d2cb7
Breno Andrade Silva <engbrenoandrade@gmail.com>
2024-03-19 14:51:16 -0300

    Update script.js

--- unifelBackEnd/script.js
+++ unifelBackEnd/script.js
@@ -1,10 +1,31 @@
-const array = ['Amaral', 7829012, 'João', 2931029, 'Regina', 2319993]
 
-const variavelQualquer = array.filter((elementoAtual) =>{
-    return elementoAtual >= 7829012
-})
 
-console.log(variavelQualquer);
+Spread objeto
+
+const info = { nome: 'Breno', age: 28 }
+const infoAdc = { lastname: 'Silva' }
+
+const pessoa = {...info, ...infoAdc}
+
+console.log(pessoa);
+
+//spread operator(...) - Junção de arrays
+//const array1 = [4, 8, 41, 2]
+//const array2 = [7, 6, 32, 1]
+
+//const array = [...array1, ...array2]
+//console.log(array)
+
+
+
+
+//const array = ['Amaral', 7829012, 'João', 2931029, 'Regina', 2319993]
+
+//const variavelQualquer = array.filter((elementoAtual) =>{
+//    return elementoAtual >= 7829012
+//})
+
+//console.log(variavelQualquer);
 
 
 
